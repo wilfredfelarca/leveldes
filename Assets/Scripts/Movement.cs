4070c9706c060365ff5a746749597280fa9525ba
Cole <202401209@iacademy.edu.ph>
2026-07-04 02:11:12 +0800

    gray boxing + Movement + Camera
    Added a script that follows the players movement
    Added Movement
    Added general areas of enemies
    Added general areas for other ants
    Added general areas for sugars

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,20 +3,17 @@ public class PlayerMover : MonoBehaviour
 {
     public float moveSpeed;
     public float sprintSpeed;
-    public float rotationSpeed = 10f; // higher = snappier turning
+    public float rotationSpeed = 20f; // higher = snappier turning
 
     void Update()
     {
-        //Assuming this is 3rd-person movement and the default Input Manager configuration is used.
+
         Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
 
-        //Use the value of "sprintSpeed" if left-shift is held down, otherwise use the value of "moveSpeed";
         float speed = Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : moveSpeed;
 
-        //Update the GameObject's position with the detected move direction and speed.
         transform.position += moveDirection * speed * Time.deltaTime;
 
-        //Rotate the model to face the direction it's moving, if there's any input.
         if (moveDirection.sqrMagnitude > 0.0001f)
         {
             Quaternion targetRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
