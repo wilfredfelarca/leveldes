1f2394468008e34afb80d34363827cdf8faf96f7
wilfredfelarca <146461547+wilfredfelarca@users.noreply.github.com>
2026-07-17 17:32:07 +0800

    added waypoint system for enemy movement

--- Assets/Scripts/DeleteCollided.cs
+++ Assets/Scripts/DeleteCollided.cs
@@ -9,7 +9,27 @@ public class DeleteCollided : MonoBehaviour
 {
     [SerializeField]
     private string tagToDelete = "Food"; // The tag of the object to delete
+    public Transform[] waypoints; // waypoints for the enemy to go
+    public float speed = 10f; // speed of enemy
+    private int currentWaypoint = 0;
 
+    void Update()
+    {
+        if (waypoints.Length == 0)
+        {
+            // prevents array going below zero
+            return; 
+        }
+        
+        // move the enemy towards waypoints
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[currentWaypoint].position, speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, waypoints[currentWaypoint].position) < 0.1f)
+        {
+            currentWaypoint = (currentWaypoint + 1) % waypoints.Length;
+        }
+    }
+    
     private void OnCollisionEnter(Collision collision)
     {
         // Only destroys the colliding object if it has the specified tag
